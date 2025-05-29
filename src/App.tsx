257c002f8dc5a90288a378ed18485c4a094964d2
tribbitts <onoki82@gmail.com>
2025-05-29 10:08:48 -0400

    Add default export to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
- 
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1>Welcome to the App</h1>
+    </div>
+  )
+}
+
+export default App
